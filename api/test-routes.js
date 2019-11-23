7fa385742ab54dc912f5a75cc4d074e0122d27fd
Travis Little <kingtrav@gmail.com>
2019-11-23 13:26:27 -0700

    more routes

--- api/test-routes.js
+++ api/test-routes.js
@@ -18,5 +18,39 @@ router.get('/accounts', (req, res) => {
         })
 })
 
+router.get('/accounts/find/id/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const account = await TestDB.findById(id)
+        res.status(200).json(account)
+    } catch(error) {
+        res.status(500).json(error)
+    }
+    
+})
+
+router.get('/accounts/find/:field/:value', async (req, res) => {
+    const { field, value } = req.params
+    const filter = {
+        field,
+        value
+    }
+    try {
+        const account = await TestDB.findBy(filter)
+        res.status(200).json(account)
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+router.post('/account/create', (req, res) => {
+    const data = req.body
+    try {
+        const newAccount = await TestDB.createAccount(data)
+        res.status(200).json(newAccount)
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
--- models/test-model.js
+++ models/test-model.js
@@ -1,9 +1,31 @@
 const db = require('../database/dbConfig.js')
 
 module.exports = {
-    find
+    find,
+    findBy,
+    findById,
+    createAccount
 }
 
 function find() {
     return db('salesforce.account')
+}
+
+function findBy(filter) {
+    return db('salesforce.account').where(filter)
+}
+
+function findById(id) {
+    return db('salesforce.account').where({id}).first()
+}
+
+function createAccount(data) {
+    return db('salesforce.account').insert(data)
+        .then(account => {
+            return account
+        })
+        .catch(error => {
+            return error
+        })
+        
 }
