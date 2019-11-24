40c094cf73e993f5098c065dcf643fc003ce0a39
Travis Little <kingtrav@gmail.com>
2019-11-24 10:24:57 -0700

    fix

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
 const config = require('../knexfile.js')
-const environment = process.env.DB_ENV || 'development'
+const environment = process.env.DB_ENV || 'production'
 
 module.exports = knex(config[environment])
