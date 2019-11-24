896bb550b40fc17925244e357d4a0925bf03b25f
Travis Little <kingtrav@gmail.com>
2019-11-24 10:35:27 -0700

    fix typo

--- api/case-routes.js
+++ api/case-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Cases = require('../models/cases-model')
+const Cases = require('../models/case-model')
 const Contacts = require('../models/Contact-model')
 const Accounts = require('../models/account-model')
 const generateConnectID = require('uniqid')
@@ -55,30 +55,5 @@ router.post('/create', async (req, res) => {
         res.status(500).json(error)
     }
 })
-
-// router.post('/create-with-account', async (req, res) => {
-//     try {
-//         const { accData, conData } = req.body
-//         // Generate unique Connect IDs for new Contact and Account records
-//         const accConnectID = generateConnectID()
-//         const conConnectID = generateConnectID()
-
-//         // Assign relational unique identifier fields
-
-//         // Account's unique Connect ID
-//         accData.merchant_account_id__c = accConnectID 
-//         // Contact's unique Connect ID
-//         conData.contact_connect_id__c = conConnectID    
-//         // Foreign key relating newly created Contact to newly created Account
-//         conData.account__merchant_account_id__c = accConnectID  
-
-//         const newData = await Cases.createAccountWithContact(accData, conData)
-//         res.status(200).json(newData)
-
-//     } catch (error) {
-//         res.status(500).json(error)
-//     }
-
-// })
 
 module.exports = router
