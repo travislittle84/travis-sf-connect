1de54df44691dbc9ddfb08fbffd283068c2cb6fb
Travis Little <kingtrav@gmail.com>
2019-11-24 17:41:41 -0700

    try this

--- models/case-model.js
+++ models/case-model.js
@@ -19,13 +19,12 @@ function findById(id) {
     return db('salesforce.case').where({id}).first()
 }
 
-function createCase(data) {
-    return db('salesforce.case').insert(data)
-        .then(newCase => {
-            return newCase
-        })
-        .catch(error => {
-            return error
-        })
-        
+async function createCase(data) {
+    try {
+        const newCase = await db('salesforce.case').insert(data)
+        return newCase
+    } catch(error) {
+        return error
+    }
+            
 }
